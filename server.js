a6aab0d57b8f890cba2c4c1278db31aefd70c1b5
daniel202521 <jairodaniwl@gmail.com>
2025-08-27 11:41:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -951,10 +951,9 @@ app.post('/api/sitio/:id/entregar', async (req, res) => {
         const id = req.params.id;
         const sitio = await sitiosCol.findOne({ _id: new ObjectId(id) });
         if (!sitio) return res.status(404).json({ error: 'Sitio no encontrado' });
-        // Elimina la verificación para permitir entregar varias veces
-        // if (sitio.entregado) {
-        //     return res.status(400).json({ error: 'El sitio ya fue entregado' });
-        // }
+        if (sitio.entregado) {
+            return res.status(400).json({ error: 'El sitio ya fue entregado' });
+        }
         await sitiosCol.updateOne(
             { _id: new ObjectId(id) },
             { $set: { entregado: true, fechaEntrega: new Date() } }
